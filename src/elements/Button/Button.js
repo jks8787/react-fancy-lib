f21200fae5b058367373f53d1c0e5428c1fafd95
jks8787 <janice.k.n.smith@gmail.com>
2017-10-17 08:24:05 -0400

    updates for new version

--- src/elements/Button/Button.js
+++ src/elements/Button/Button.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Button extends Component {
   render() {
     return (
-      <button className="button is-primary">
+      <button className="button is-info">
         {'i am a button'}
       </button>
     );
